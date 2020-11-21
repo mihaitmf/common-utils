2e33d6640988f738b6519340b02563239658b5d8
mihaitmf <mihai.tmf@gmail.com>
2020-11-21 03:29:41 +0200

    Add tests for ExecutionStatistics with static method

--- test/Command/ExecutionStatisticsTest.php
+++ test/Command/ExecutionStatisticsTest.php
@@ -14,7 +14,7 @@ class ExecutionStatisticsTest extends TestCase
         $this->executionStats = new ExecutionStatistics();
     }
 
-    public function testCalculateStatsSuccess(): void
+    public function testCalculateStatsSuccessUsingStartEnd(): void
     {
         $secondsSleep = 0.01;
         $this->executionStats->start();
@@ -25,14 +25,28 @@ class ExecutionStatisticsTest extends TestCase
         self::assertSame("\nExecution time: 0.0101 seconds\nMemory peak usage: 6.00 MB\n", $statsMessage);
     }
 
-    public function testCalculateStatsWithoutInitialization(): void
+    public function testCalculateStatsSuccessUsingStaticMethod(): void
+    {
+        $secondsSleep = 0.005;
+        ob_start();
+
+        $startTime = microtime(true);
+        usleep($secondsSleep * 1000 * 1000);
+        ExecutionStatistics::printStats($startTime);
+
+        $statsMessage = ob_get_clean();
+
+        self::assertSame("\nExecution time: 0.0051 seconds\nMemory peak usage: 6.00 MB\n", $statsMessage);
+    }
+
+    public function testCalculateStatsFailWithoutInitialization(): void
     {
         $statsMessage = $this->executionStats->getPrintMessage();
 
         self::assertSame("\nExecution time: 0.0000 seconds\nMemory peak usage: 0.00 MB\n", $statsMessage);
     }
 
-    public function testCalculateStatsWithoutFinalization(): void
+    public function testCalculateStatsFailWithoutFinalization(): void
     {
         $this->executionStats->start();
         $statsMessage = $this->executionStats->getPrintMessage();
