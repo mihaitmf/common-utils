0e0ca336d74423f3440624aa2e560c5dcbf1893a
mihaitmf <mihai.tmf@gmail.com>
2021-05-06 14:21:56 +0300

    Unit tests for DI Container

--- test/DI/ContainerTest.php
+++ test/DI/ContainerTest.php
@@ -8,11 +8,13 @@ use CommonUtils\Tests\DI\TestClass\TestClassWithConstructorDependency;
 use CommonUtils\Tests\DI\TestClass\TestClassWithConstructorDependencyChain;
 use CommonUtils\Tests\DI\TestClass\TestClassWithoutDependencies;
 use CommonUtils\Tests\DI\TestClass\TestClassWithStringDependency;
+use CommonUtils\Tests\DI\TestClass\TestInterface;
+use DI\NotFoundException;
 use PHPUnit\Framework\TestCase;
 
 class ContainerTest extends TestCase
 {
-    private const DEFINITIONS_FILE_PATH = __DIR__ . DIRECTORY_SEPARATOR . 'TestClass' . DIRECTORY_SEPARATOR . 'di-config.php';
+    private const DEFINITIONS_FILE_PATH = __DIR__ . DIRECTORY_SEPARATOR . 'TestClass' . DIRECTORY_SEPARATOR . 'test-di-config.php';
 
     public function testContainerGetSuccess(): void
     {
@@ -86,5 +88,23 @@ class ContainerTest extends TestCase
             $object->getName(),
             'Container::get did not return the correct instance'
         );
+    }
+
+    public function testContainerGetInexistentClassFail(): void
+    {
+        $this->expectException(NotFoundException::class);
+        $this->expectExceptionMessage('No entry or class found for');
+
+        Container::get('ThisClassDoesNotExist');
+    }
+
+    public function testContainerGetInterfaceWithoutDefinitionFail(): void
+    {
+        Container::get(TestInterface::class);
+    }
+
+    public function testContainerGetClassWithPrimitiveDependencyWithoutDefinitionFail(): void
+    {
+        self::markTestIncomplete('TODO');
     }
 }
